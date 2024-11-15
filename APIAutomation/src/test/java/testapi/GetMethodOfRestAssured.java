c11f99ab5bd0e69908a59c2f678b6aaf5ff41e6c
vips-svg <vips@svg>
2024-11-15 14:03:53 +0530

    pushing 1st api project and this is first commit

---
+++ APIAutomation/src/test/java/testapi/GetMethodOfRestAssured.java
@@ -0,0 +1,17 @@
+package testapi;
+
+import static io.restassured.RestAssured.get;
+import io.restassured.response.Response;
+
+public class GetMethodOfRestAssured {
+
+	public static void main(String[] args) {
+				
+		Response res = get("https://reqres.in/api/users?page=2");
+		
+        System.out.println(res.getStatusCode());
+		System.out.println( res.getBody().asPrettyString());
+	
+	}
+
+}
